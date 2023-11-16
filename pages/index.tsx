a262b860662c760959e425b36a75a22625ee4465
Chet Woodside <chet@cosmicblend.ca>
2023-11-16 06:48:47 -0800

    first push

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,27 @@
-// import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import HeaderNav from '../frontend/components/headernav';
 import ArtWallList from '../frontend/components/artwalllist';
+import { 
+  Heading
+} from '@chakra-ui/react';
 // import { useAccount } from 'wagmi';
 // import React, { useState, useEffect } from 'react';
 // import { WalletClient } from 'viem'; // Import WalletClient from viem
 
 const Home: NextPage = () => {
-  //const { address } = useAccount();
-
   return (
     <div>
       <Head>
-        <title>1155 skeleton for zora</title>
-        <meta content="1155 skeleton for zora" name="1155 skeleton for zora" />
+        <title>Technology of Togetherness by Take Up Space</title>
+        <meta content="Technology of Togetherness by Take Up Space" name="Technology of Togetherness" />
         <link href="/favicon.ico" rel="icon" />
       </Head>
 
       <HeaderNav />
 
       <main>
+        <Heading>The Artists</Heading>
         <ArtWallList />
       </main>
 
