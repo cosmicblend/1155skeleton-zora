6f75345e253712bd636bb102a685c118f0be759a
Chet Woodside <chet@cosmicblend.ca>
2023-11-17 13:56:49 -0800

    introducing error handling and spin that cd

--- frontend/components/headernav.tsx
+++ frontend/components/headernav.tsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { 
     Flex,  
     Text, 
     Box,
-    Image
+    Image,
+    HStack
 } from '@chakra-ui/react';
 
 const HeaderNav = () => {
-    const logoUrl = '/images/tus_logo.png'; 
+    const [scrollVelocity, setScrollVelocity] = useState(0);
+    const [lastScrollTop, setLastScrollTop] = useState(0);
+    const [lastTime, setLastTime] = useState(Date.now());
+
+    const handleScroll = () => {
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        const now = Date.now();
+        const velocity = Math.abs(scrollTop - lastScrollTop) / (now - lastTime);
+
+        setScrollVelocity(velocity);
+        setLastScrollTop(scrollTop);
+        setLastTime(now);
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [lastScrollTop, lastTime]);
+
+    const logoUrl = '/images/tot-brand.png'; 
     const totUrl = '/images/cursivish-tot.png';
 
     return (
@@ -25,24 +47,28 @@ const HeaderNav = () => {
          py={2}
          align='center'
          justify='space-between'
+         boxShadow="0 0 0.062rem rgba(0, 0, 0, 0.38)"
         >
             <Box flex="1">
-                <Image
-                    src={logoUrl}
-                    width='3.999rem'
-                    alt=''
-                    borderRadius='0'
-                    textAlign="center"
-                />
-            </Box>
-            <Box>
-                <Image
-                    src={totUrl}
-                    height='3.998rem'
-                    alt=''
-                    borderRadius='0'
-                    textAlign="center"
-                /> 
+            <HStack>
+                    <Image
+                        src={logoUrl}
+                        height='3.999rem'
+                        alt=''
+                        borderRadius='0'
+                        textAlign="center"
+                        style={{ transform: `rotate(${scrollVelocity * 1080}deg)` }}
+                    />
+                    <Image
+                        src={totUrl}
+                        height={['1.999rem', '2.827rem']}
+                        alt=''
+                        borderRadius='0'
+                        textAlign="center"
+                        ml='-1rem'
+                    /> 
+                </HStack>
+            </Box><Box>
             </Box>
             <Box flex="1" display="flex" justifyContent="flex-end">
                 <ConnectButton />
--- frontend/components/mintButton.tsx
+++ frontend/components/mintButton.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import { Button } from "@chakra-ui/react";
+import React, { useState } from 'react';
+import { Button, useToast } from "@chakra-ui/react";
 import { createMintClient } from "@zoralabs/protocol-sdk";
 import type { Address, WalletClient } from "viem";
 import { createPublicClient, createWalletClient, http } from "viem";
 import { zora } from "viem/chains";
 import { useWalletClient } from "wagmi";
+import { useConnectModal } from '@rainbow-me/rainbowkit';
 
 async function mintNFT(
   walletClient: WalletClient,
@@ -45,18 +46,65 @@ function MintButton() {
   const address = "0x0250FfD918AA6DeeA1bbBD7B89DbA06478E723be";
   const tokenId = BigInt(1);
   const walletClient = useWalletClient();
+  const { openConnectModal } = useConnectModal();
+  const isWalletConnected = walletClient?.data !== undefined;
+  const [isMinting, setIsMinting] = useState(false);
+  const toast = useToast();
 
-  const handleMint = () => {
-    if (!walletClient?.data) {
-      return;
+  const handleMint = async () => {
+    if (walletClient?.data) {
+      setIsMinting(true); // Start minting
+      try {
+        await mintNFT(walletClient.data, address, tokenId);
+        toast({
+          title: "Success",
+          description: "NFT minted successfully!",
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+          position: "top",
+        });
+      } catch (error) {
+        let errorMessage = "Something went wrong. Please try again.";
+        let errorTitle = "Error";        
+        
+      // Check for a transaction rejection error
+      if (error instanceof Error && error.message.includes("User denied transaction signature")) {
+        errorMessage = "Transaction rejected by user.";
+        errorTitle = "Transaction Rejected";
+      }
+      toast({
+        title: "Error",
+        description: errorMessage,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "top",
+      });
+      } finally {
+        setIsMinting(false); // End minting
+      }
+    } else {
+      if (openConnectModal) {
+        openConnectModal();
+      } else {
+        // Handle the case where openConnectModal is undefined
+        console.error("Wallet connection functionality is unavailable.");
+        toast({
+          title: "Error",
+          description: "Unable to connect to the wallet. Please try again later.",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+          position: "top",
+        });
+      }
     }
-
-    mintNFT(walletClient.data, address, tokenId);
-  };
+  };  
 
   return (
     <Button colorScheme="blue" onClick={handleMint}>
-      Mint NFT
+      {isWalletConnected ? (isMinting ? "Minting..." : "Mint") : "Connect Your Wallet"}
     </Button>
   );
 }
